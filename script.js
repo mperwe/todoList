466099f003b35088509929253d99966a18ab2dfe
Frank Mperwe Kyambadde <frankmperwe@gmail.com>
2023-07-10 02:30:13 +0200

    editing to do item in the list

--- script.js
+++ script.js
@@ -30,53 +30,14 @@ function displayToView(){
     const todoListItems = document.createElement('ul') 
 // loop over array from localstorage
    for(let i=0;i<todoList.length;i++){
-      const listItem = document.createElement("li")
-// creating parent div
-    const divEl = document.createElement("div")
-    divEl.className = "textDiv"
-        
-// create a checkbox
-      const checkbox = document.createElement("input")
-      checkbox.type = 'checkbox'
-
-    // add checkbox eventListener
-    checkbox.addEventListener("click", function(){
-       removeListItem(todoList[i], listItem)
-    })
-      divEl.appendChild(checkbox)
-// create text span
-    const textSpan = document.createElement("span")
-    textSpan.textContent = todoList[i]
-    divEl.appendChild(textSpan)
-
-    listItem.appendChild(divEl) // appending to the listItem
-
-//create parent Div for icons
-    const iconDiv= document.createElement("div")
-    iconDiv.className = "iconDiv"
-
-// create edit icon
-    const editIcon = document.createElement("i") 
-    editIcon.className = "fa fa-pencil-square-o" 
-    iconDiv.appendChild(editIcon)      
-
-// create delete icon
-    const deleteIcon = document.createElement("i")
-    deleteIcon.className ="fa fa-trash"
-
-// add Delete eventListener
-    deleteIcon.addEventListener("click", function(){
-        removeListItem(todoList[i], listItem)
-     })
-    iconDiv.appendChild(deleteIcon)
-    listItem.appendChild(iconDiv)  
-    // append listItem to todoListItems UL
+    const listItem = addListItem(todoList[i])
+// append listItem to todoListItems UL
     todoListItems.appendChild(listItem)
-            }
 // append the UL to the div element
-        todoDiv.appendChild(todoListItems)
+    todoDiv.appendChild(todoListItems)
 }
 
+}
 //display items on pageload
 window.onload = function(){
     displayToView()
@@ -93,5 +54,87 @@ function removeListItem(item, listItem){
    }
 
    listItem.remove()
-
 }
+// function edit task
+function editListItem(item, listItem){
+    // create an input field
+    const inputField = document.createElement("input")
+    inputField.type = "text"
+    inputField.value = item
+    inputField.className = "edit-input"
+
+    // replace the text span with input field
+    listItem.replaceWith(inputField)
+
+    // focus the input field
+    inputField.focus()
+
+    //Add eventListerner to save changes on enter
+    inputField.addEventListener('keydown', function(event){
+        if(event.key ==="Enter"){
+            const updatedItem = inputField.value
+    // update Item in Local Storage
+           const storedData = localStorage.getItem("todoList")
+           const parsedData = JSON.parse(storedData) 
+           const index = parsedData.indexOf(item)
+           if(index>-1){
+            parsedData[index]=updatedItem
+            localStorage.setItem("todoList", JSON.stringify(parsedData))
+           }
+    // replacing the input field with updated list Item
+         const newListItem = addListItem(updatedItem)
+         inputField.replaceWith(newListItem)
+        }
+    })
+}
+
+
+// function to create listItem and add it to unorderlist
+function addListItem(item){
+    const listItemElement = document.createElement("li")
+    // creating parent div
+        const divEl = document.createElement("div")
+        divEl.className = "textDiv"
+            
+    // create a checkbox
+          const checkbox = document.createElement("input")
+          checkbox.type = 'checkbox'
+    
+        // add checkbox eventListener
+        checkbox.addEventListener("click", function(){
+           removeListItem(item, listItemElement)
+        })
+          divEl.appendChild(checkbox)
+    // create text span
+        const textSpan = document.createElement("span")
+        textSpan.textContent = item
+        divEl.appendChild(textSpan)
+    
+        listItemElement.appendChild(divEl) // appending to the listItem
+    
+    //create parent Div for icons
+        const iconDiv= document.createElement("div")
+        iconDiv.className = "iconDiv"
+    
+    // create edit icon
+        const editIcon = document.createElement("i")
+        editIcon.className = "fa fa-pencil-square-o"
+    
+    // add Edit eventListener
+        editIcon.addEventListener("click", function(){
+            editListItem(item, listItemElement)
+         }) 
+        iconDiv.appendChild(editIcon)      
+    
+    // create delete icon
+        const deleteIcon = document.createElement("i")
+        deleteIcon.className ="fa fa-trash"
+    
+    // add Delete eventListener
+        deleteIcon.addEventListener("click", function(){
+            removeListItem(item, listItemElement)
+         })
+        iconDiv.appendChild(deleteIcon)
+        listItemElement.appendChild(iconDiv)  
+        return listItemElement
+                }
